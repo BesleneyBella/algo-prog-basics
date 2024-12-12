6f25ef47a239e4fa5f585ef7e8186c0a5d26176c
BesleneyBella <arabella.besleney@gmail.com>
2024-12-12 23:48:37 +0300

    Create 16.jl

---
+++ 16.jl
@@ -0,0 +1,21 @@
+using HorizonSideRobots
+robot = Robot(animate=true)
+inverse(side::HorizonSide)=HorizonSide((Int(side)+2)%4)
+function HorizonSideRobots.move!(robot::Robot, side, steps::Int) 
+    while steps>0
+        move!(robot,side)
+        steps-=1
+    end
+end
+function main!(robot)
+    shuttle!(()->!isborder(robot,Nord),robot,Ost)
+end
+
+function shuttle!(stop_condition::Function, robot,side::HorizonSide)
+    n = 0
+    while !stop_condition()
+        move!(robot, side, n)
+        side = inverse(side)
+        n += 1
+    end
+end
